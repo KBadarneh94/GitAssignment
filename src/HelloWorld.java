c682f1bf27f76ec75a83a4f954336cc4551f5378
khaled <badarneh484@gmail.com>
2024-06-22 02:06:49 +0300

    Apply hotfix 1.0.1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is Khaled Badarneh - Atypon. Release 1.0");
+        System.out.println("This is Khaled Badarneh - Atypon. Hotfix 1.0.1");
     }
 }
