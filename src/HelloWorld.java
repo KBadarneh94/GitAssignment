6d07c10460dc8d0abf081be10058210084a70cbe
khaled <badarneh484@gmail.com>
2024-06-22 02:05:55 +0300

    Prepare for release 1.0

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is Khaled Badarneh - Atypon. This is feature2.");
+        System.out.println("This is Khaled Badarneh - Atypon. Release 1.0");
     }
 }
