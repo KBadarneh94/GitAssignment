bc288e990a9e38808b45ef1612ba7dee1cc95464
khaled <badarneh484@gmail.com>
2024-06-22 02:03:23 +0300

    Add feature1 changes to HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is Khaled Badarneh - Atypon");
+        System.out.println("This is Khaled Badarneh - Atypon. This is feature1.");
     }
 }
